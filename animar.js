a5cf51dd82b0ebc25ad567569179b963d01473af
Bruno Victor <1pontosde1@gmail.com>
2022-10-22 23:06:44 -0300

    A Great Step
    .Transformando da água em vinho, realmente;
    .Adicionei algumas ideias que tive para o projeto, e achei bem legal esse processo de exploração e solução de problemas;
    .Falando em problemas, resolvi todos os problemas relacionados à lógica do algoritmo. As operações, tanto crescentes quanto decrescentes, estão funcionando perfeitamente e sem nenhum problema aparente;
    .No mais... o projeto está (parcialmente) finalizado, mas ainda assim gostaria de investigar melhor uma forma de ajustar o tamanho desses inputs desgraçados... vou por isso na lista pra tentar resolver e aprender.

---
+++ animar.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt('.animated',{
+        speed: 100,
+        strings: '...',
+        loop: true,
+    }).go()
+})
--- script.js
+++ script.js
@@ -1,39 +1,115 @@
 function justCounting(){
-    var i = window.document.querySelector('input#iinicio') // Início
-    var f = window.document.querySelector('input#ifim') // Fim
-    var p = window.document.querySelector('input#ipasso') // Passo
+    var inicio = window.document.querySelector('input#iinicio') // ...
+    var i =  Number(inicio.value)// Início
+    //
+    var fim = window.document.querySelector('input#ifim') // ...
+    var f =  Number(fim.value)// Fim
+    //
+    var passo = window.document.querySelector('input#ipasso') // ...
+    var p =  Number(passo.value)// Passo
+    //
     var b = window.document.querySelector('input#botaoenvio') // Botão
     var con = window.document.querySelector('div#contas') // Contando
-    var c = i
-    var p = 1
+    var c = i // Contador (Início)
 
-    if (i.value = null){
+    if (inicio.value.length == 0 || fim.value.length == 0){
         window.alert ('[Erro] Tente Novamente!')
         con.innerHTML('Impossível contar... Tente outra numeração.')
 
-    } else if (p.value = 0) {
-        window.alert ('[Erro] Tente Novamente!')
-        while (c<=f) {
-            i++
+    } else if (passo.value.length == 0 || Number(passo.value)==0 || p<=0) {
+        window.alert ('[Erro] Considamos o passo como 1!')
+        if (f>c){
+            while (c<=f) {
+                con.innerHTML += `${c}... `
+                p=1
+                c+=p
+            }
+        } 
+
+        else {
+            while (c>=f) {
+                con.innerHTML += `${c}... `
+                p=1
+                c-=p
+            }
         }
     }
+
     //--------------------------------------------------------------
     
     else {
-        if (i.value > 0 && f>0){
+        con.innerHTML = `<strong>Contados!<strong><br>`
+        
+        if (f>c){
             while (c<=f) {
-                i++
-                con.innerHTML = `${i}`
+               con.innerHTML += `${c}... ` 
+               c+=p
             } 
         } 
 
         else {
             while (c>=f) {
-                i--
-                con.innerHTML = `${i}`
+                con.innerHTML += `${c}... `
+                c-=p 
             }
         }
-    } 
-
-    con.innerHTML = `${i}`
+    }  
 }
+
+/*
+function justCounting(){
+    var inicio = window.document.querySelector('input#iinicio') // Início
+    
+    //
+    var fim = window.document.querySelector('input#ifim') // Fim
+    
+    //
+    var passo = window.document.querySelector('input#ipasso') // Passo
+    
+    //
+    var b = window.document.querySelector('input#botaoenvio') // Botão
+    var con = window.document.querySelector('div#contas') // Contando
+    var c = i // Contador (Início)
+
+    if (inicio.value.length == 0 || fim.value.length == 0){
+        window.alert ('[Erro] Tente Novamente!')
+        con.innerHTML('Impossível contar... Tente outra numeração.')
+
+    } else if (passo.value.length == 0 || Number(passo.value)==0) {
+        window.alert ('[Erro] Considamos o passo como 1!')
+
+        let i =  Number(inicio.value)// Início
+        let f =  Number(fim.value)// Fim
+        let p =  Number(passo.value)// Passo
+
+        while (c<=f) {
+            con.innerHTML += `${c}`
+            p=1
+            c+=p
+            
+        }
+    }
+
+    //--------------------------------------------------------------
+    
+    else {
+        let i =  Number(inicio.value)// Início
+        let f =  Number(fim.value)// Fim
+        let p =  Number(passo.value)// Passo
+
+        if (f>0){
+            while (c<=f) {
+                con.innerHTML += `${c}` 
+                c+=p
+            } 
+        } 
+
+        else {
+            while (c>=f) {
+                con.innerHTML += `${c}`
+                c-=p 
+            }
+        }
+    }  
+}
+*/
